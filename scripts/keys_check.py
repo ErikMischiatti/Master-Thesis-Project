aebbb7c11826e2d4a0c99a6478471f46df6d7a3e
Erik <77512702+ErikMischiatti@users.noreply.github.com>
2024-10-31 13:48:22 +0100

    Delete scripts directory

--- scripts/keys_check.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-file_path = '/home/asl_team/catkin_ws/src/dhb_ros-General/data/dhb/generated_traj_dhb_vel_push_pos1_ee_velocities.npz'  # Choose the .npz file to check
-data = np.load(file_path)
-print(data.files)  
