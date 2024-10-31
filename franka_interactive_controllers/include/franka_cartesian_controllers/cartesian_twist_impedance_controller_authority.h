4610b6014abee4a3e702eb0fda47b20ac3653812
Erik <77512702+ErikMischiatti@users.noreply.github.com>
2024-10-31 13:58:29 +0100

    Add files via upload

---
+++ franka_interactive_controllers/include/franka_cartesian_controllers/cartesian_twist_impedance_controller_authority.h
@@ -0,0 +1,88 @@
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <controller_interface/multi_interface_controller.h>
+#include <dynamic_reconfigure/server.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+#include <Eigen/Dense>
+
+#include <franka_interactive_controllers/compliance_paramConfig.h>
+#include <franka_hw/franka_model_interface.h>
+#include <franka_hw/franka_state_interface.h>
+
+#include <std_msgs/Float64.h>
+
+namespace franka_interactive_controllers {
+
+class CartesianTwistImpedanceControllerAuthority : public controller_interface::MultiInterfaceController<
+                                                franka_hw::FrankaModelInterface,
+                                                hardware_interface::EffortJointInterface,
+                                                franka_hw::FrankaStateInterface> {
+ public:
+  bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& node_handle) override;
+  void starting(const ros::Time&) override;
+  void update(const ros::Time&, const ros::Duration& period) override;
+
+ private:
+  // Saturation
+  Eigen::Matrix<double, 7, 1> saturateTorqueRate(
+      const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
+      const Eigen::Matrix<double, 7, 1>& tau_J_d);  // NOLINT (readability-identifier-naming)
+
+  std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
+  std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
+  std::vector<hardware_interface::JointHandle> joint_handles_;
+
+  double dt_ = 0.001;
+  double filter_params_{0.005};
+  double nullspace_stiffness_{20.0};
+  const double delta_tau_max_{1.0};
+  Eigen::Matrix<double, 6, 6> cartesian_stiffness_;
+  Eigen::Matrix<double, 6, 6> cartesian_damping_;
+  Eigen::Matrix<double, 7, 1> q_d_nullspace_;
+  
+  // whether to load from yaml or use initial robot config
+  bool q_d_nullspace_initialized_ = false;
+  Eigen::Vector3d position_d_;
+  Eigen::Quaterniond orientation_d_;
+  Eigen::Vector3d position_d_target_;
+  Eigen::Quaterniond orientation_d_target_;
+  Eigen::Vector3d velocity_d_;
+
+  double K_trans, D_trans, K_rot, D_rot;
+  double K_R_trans  = 800.0;//1500.0;
+  double K_H_trans  = 1.0;
+  double K_R_rot    = 0.03;//50.0;//50.0;
+  double K_H_rot    = 0.03;
+
+  // Timing
+  ros::Duration elapsed_time;
+  double last_cmd_time;
+  double vel_cmd_timeout;
+
+
+  // Variables for initialization and tool compensation
+  Eigen::Matrix<double, 6, 1> tool_compensation_force_;
+  bool activate_tool_compensation_;
+  bool update_impedance_params_;
+
+
+  // Desired twist subscriber
+  ros::Subscriber sub_desired_twist_;
+  ros::Subscriber sub_alpha_h_;
+  void alphaHCallback(const std_msgs::Float64::ConstPtr& msg);
+
+  void desiredTwistCallback(const geometry_msgs::TwistConstPtr& msg);
+};
+
+}  // namespace franka_interactive_controllers
