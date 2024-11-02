cbdfb3405cd5941a255d0fd16463a12fd03558a2
Erik <77512702+ErikMischiatti@users.noreply.github.com>
2024-11-02 14:39:52 +0100

    Update aruco_trackbars.py

--- DLO_detection/aruco_orientation/aruco_trackbars.py
+++ DLO_detection/aruco_orientation/aruco_trackbars.py
@@ -11,11 +11,10 @@ def nothing(x):
 
 def setup_trackbars():
     """Setup OpenCV trackbars for HSV parameters."""
-    cv2.namedWindow('Parameters', cv2.WINDOW_NORMAL)  # Creazione della finestra "Parameters"
+    cv2.namedWindow('Parameters', cv2.WINDOW_NORMAL)
     sections = ['Connector', 'Cable']
 
     for section in sections:
-        # Creazione delle trackbars per ogni sezione (Connector e Cable)
         cv2.createTrackbar(f'LH {section}', 'Parameters', 0, 179, nothing)
         cv2.createTrackbar(f'LS {section}', 'Parameters', 0, 255, nothing)
         cv2.createTrackbar(f'LV {section}', 'Parameters', 0, 255, nothing)
@@ -23,14 +22,11 @@ def setup_trackbars():
         cv2.createTrackbar(f'US {section}', 'Parameters', 255, 255, nothing)
         cv2.createTrackbar(f'UV {section}', 'Parameters', 255, 255, nothing)
 
-    # print("Trackbars have been set up.")  # Debug message
-
 def load_saved_params(params):
     """Load saved HSV parameters into trackbars."""
     sections = ['Connector', 'Cable']
 
     if params:
-        # print(f"Loading saved parameters: {params}")  # Debug message
         for section in sections:
             cv2.setTrackbarPos(f'LH {section}', 'Parameters', params[f'LH_{section}'])
             cv2.setTrackbarPos(f'LS {section}', 'Parameters', params[f'LS_{section}'])
@@ -38,9 +34,7 @@ def load_saved_params(params):
             cv2.setTrackbarPos(f'UH {section}', 'Parameters', params[f'UH_{section}'])
             cv2.setTrackbarPos(f'US {section}', 'Parameters', params[f'US_{section}'])
             cv2.setTrackbarPos(f'UV {section}', 'Parameters', params[f'UV_{section}'])
-    # else:
-    #     # print("No saved parameters found.")  # Debug message
-
+            
 def get_trackbar_values():
     """Retrieve current HSV values from trackbars."""
     values = {}
@@ -54,5 +48,4 @@ def get_trackbar_values():
         values[f'u_s_{section.lower()}'] = cv2.getTrackbarPos(f'US {section}', 'Parameters')
         values[f'u_v_{section.lower()}'] = cv2.getTrackbarPos(f'UV {section}', 'Parameters')
 
-    # print(f"Current HSV values: {values}")  # Debug message
     return values
